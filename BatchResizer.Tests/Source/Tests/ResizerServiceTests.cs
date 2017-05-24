18f2cce9df8c7baa9f9c7776fbefae20497f9b0b
Alex <dwt416@gmail.com>
2017-05-24 22:07:23 +0300

    Added tests project

---
+++ BatchResizer.Tests/Source/Tests/ResizerServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace BatchResizer.Tests
+{
+    [TestFixture]
+    public class ResizerServiceTests
+    {
+        [Test]
+        public void TodoTest()
+        {
+            // TODO: add proper unit testing
+        }
+    }
+}
